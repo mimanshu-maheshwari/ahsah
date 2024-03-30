2ab1f171903df8c66dba2bcfc744fb4c26f5f30a
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-03-31 01:08:55 +0530

    upate main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::{fs::File, io::Read};
-
+use std::fs;
 use ahsah::{hashes::AhsahHasher, sha256::Sha256};
 
 fn main() {
-    let mut file = match File::open("res/test.txt") {
-        Ok(file) => file, 
-        Err(err) => panic!("Unable to read file, {}", err),
-    };
     let mut hasher = Sha256::new();
-    let mut buf = Vec::new();
-    let bytes = file.read(&mut buf).unwrap();
-    println!("{bytes} read of file");
-    hasher.digest(&buf);
+    let file_path = "res/poem.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    hasher.digest(&contents.as_bytes());
     println!("{}", hasher.finish());
 }
