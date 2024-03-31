c73d0deeb26ee5655412babb1e01841bbb18993d
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-03-31 14:24:18 +0530

    Add non working implementation of sha512

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod hashes;
 pub mod sha256;
+pub mod sha512;
 pub mod utils;
--- src/sha512.rs
+++ src/sha512.rs
@@ -4,39 +4,48 @@ use std::fmt::LowerHex;
 use std::ops::{BitOr, BitXor, Shl, Shr};
 
 /// Message buffer size in bits
-const BUFFER_SIZE_BITS: usize = 512;
+const BUFFER_SIZE_BITS: usize = 1024;
 /// Message buffer size in bytes
 const BUFFER_SIZE_U8: usize = BUFFER_SIZE_BITS / 8;
-const BUFFER_SIZE_U32: usize = BUFFER_SIZE_BITS / 32;
+const BUFFER_SIZE_U64: usize = BUFFER_SIZE_BITS / 64;
 
-const MESSAGE_SCHEDULE_SIZE: usize = 64;
+const MESSAGE_SCHEDULE_SIZE: usize = 80;
 
 /// Hash size in 4bytes
-const HASH_SIZE_BITS: usize = 256;
-const HASH_SIZE_U32: usize = HASH_SIZE_BITS / 32;
+const HASH_SIZE_BITS: usize = 512;
+const HASH_SIZE_U64: usize = HASH_SIZE_BITS / 64;
 
-const LENGTH_VALUE_PADDING_SIZE_BITS: usize = 64;
+const LENGTH_VALUE_PADDING_SIZE_BITS: usize = 128;
 
-const H: [u32; HASH_SIZE_U32] = [
-    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+const H: [u64; HASH_SIZE_U64] = [
+    0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 
+    0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
 ];
 
-const K: [u32; BUFFER_SIZE_U8] = [
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+const K: [u64; MESSAGE_SCHEDULE_SIZE] = [
+    0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538, 
+    0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe, 
+    0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 
+    0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65, 
+    0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5, 0x983e5152ee66dfab, 
+    0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725, 
+    0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 
+    0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b, 
+    0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218, 
+    0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 
+    0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 
+    0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec, 
+    0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c, 
+    0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6, 
+    0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 
+    0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
 ];
 
-pub struct Sha256 {
+pub struct Sha512 {
     data: Vec<u8>,
 }
 
-impl Sha256 {
+impl Sha512 {
 
     pub fn new() -> Self {
         Self { data: Vec::new() }
@@ -47,16 +56,16 @@ impl Sha256 {
     }
 
     fn compression(
-        w: &[u32; MESSAGE_SCHEDULE_SIZE],
+        w: &[u64; MESSAGE_SCHEDULE_SIZE],
         (a, b, c, d, e, f, g, h): (
-            &mut u32,
-            &mut u32,
-            &mut u32,
-            &mut u32,
-            &mut u32,
-            &mut u32,
-            &mut u32,
-            &mut u32,
+            &mut u64,
+            &mut u64,
+            &mut u64,
+            &mut u64,
+            &mut u64,
+            &mut u64,
+            &mut u64,
+            &mut u64,
         ),
     ) {
         for i in 0..MESSAGE_SCHEDULE_SIZE {
@@ -90,7 +99,7 @@ impl Sha256 {
             + Clone
             + Copy,
     {
-        right_rotate(x, 2) ^ right_rotate(x, 13) ^ right_rotate(x, 22)
+        right_rotate(x, 28) ^ right_rotate(x, 34) ^ right_rotate(x, 39)
     }
 
     ///Σ1 will work on e
@@ -103,7 +112,7 @@ impl Sha256 {
             + Clone
             + Copy,
     {
-        right_rotate(x, 6) ^ right_rotate(x, 11) ^ right_rotate(x, 25)
+        right_rotate(x, 14) ^ right_rotate(x, 18) ^ right_rotate(x, 41)
     }
 
     /// σ0 will work on
@@ -116,7 +125,7 @@ impl Sha256 {
             + Clone
             + Copy,
     {
-        right_rotate(x, 7) ^ right_rotate(x, 18) ^ right_shift(x, 3)
+        right_rotate(x, 1) ^ right_rotate(x, 8) ^ right_shift(x, 7)
     }
 
     /// σ1 will work on
@@ -129,7 +138,7 @@ impl Sha256 {
             + Clone
             + Copy,
     {
-        right_rotate(x, 17) ^ right_rotate(x, 19) ^ right_shift(x, 10)
+        right_rotate(x, 19) ^ right_rotate(x, 61) ^ right_shift(x, 6)
     }
     #[allow(unused)]
     fn print_buf<T>(buf: &[T]) -> ()
@@ -167,17 +176,22 @@ impl Sha256 {
     /// will copy the data of u8 vec into a array of u32.
     /// we can assert for u8_block remaining bytes length but it should not fail because we
     /// will be padding the vec before copying the data int buffer block.
-    fn copy_buf_u8_to_u32(u8_block: &[u8], u32_block: &mut [u32; BUFFER_SIZE_U32], start: usize) {
+    fn copy_buf_u8_to_u64(data: &[u8], block: &mut [u64; BUFFER_SIZE_U64], start: usize) {
         assert!(
-            BUFFER_SIZE_U8 <= u8_block.len() - start,
+            BUFFER_SIZE_U8 <= data.len() - start,
             "Remaining bits in buffer are {val}, Expected {BUFFER_SIZE_U8} bits",
-            val = (u8_block.len() - start)
+            val = (data.len() - start)
         );
-        for i in 0..BUFFER_SIZE_U32 {
-            u32_block[i] = (u8_block[start + (i * 4) + 0] as u32) << 24
-                | (u8_block[start + (i * 4) + 1] as u32) << 16
-                | (u8_block[start + (i * 4) + 2] as u32) << 8
-                | (u8_block[start + (i * 4) + 3]) as u32;
+        for i in 0..BUFFER_SIZE_U64 {
+            block[i] = 
+                  (data[start + (i * 4) + 0] as u64) << 56
+                | (data[start + (i * 4) + 1] as u64) << 48
+                | (data[start + (i * 4) + 2] as u64) << 40
+                | (data[start + (i * 4) + 3] as u64) << 32
+                | (data[start + (i * 4) + 4] as u64) << 24
+                | (data[start + (i * 4) + 5] as u64) << 16
+                | (data[start + (i * 4) + 6] as u64) << 8
+                |  data[start + (i * 4) + 7] as u64;
         }
     }
 
@@ -191,18 +205,27 @@ impl Sha256 {
     }
 
     fn copy_len_to_buf(temp_block_buf: &mut Vec<u8>, len: usize) {
-        temp_block_buf.push((len >> 56) as u8 & 0xff);
-        temp_block_buf.push((len >> 48) as u8 & 0xff);
-        temp_block_buf.push((len >> 40) as u8 & 0xff);
-        temp_block_buf.push((len >> 32) as u8 & 0xff);
-        temp_block_buf.push((len >> 24) as u8 & 0xff);
-        temp_block_buf.push((len >> 16) as u8 & 0xff);
-        temp_block_buf.push((len >> 8) as u8 & 0xff);
-        temp_block_buf.push((len >> 0) as u8 & 0xff);
+        let len = len as u128;
+        temp_block_buf.push((len >> 120u128) as u8 & 0xff);
+        temp_block_buf.push((len >> 112u128) as u8 & 0xff);
+        temp_block_buf.push((len >> 104u128) as u8 & 0xff);
+        temp_block_buf.push((len >> 96u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 88u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 80u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 72u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 64u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 56u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 48u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 40u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 32u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 24u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 16u128 ) as u8 & 0xff);
+        temp_block_buf.push((len >> 8u128  ) as u8 & 0xff);
+        temp_block_buf.push((len >> 0u128  ) as u8 & 0xff);
     }
 }
 
-impl AhsahHasher for Sha256 {
+impl AhsahHasher for Sha512 {
     fn digest(&mut self, data: &[u8]) {
             for byte in data {
                 self.data.push(byte.clone());
@@ -216,7 +239,7 @@ impl AhsahHasher for Sha256 {
         // println!("INFO: Recived message of length: {msg_len}");
 
         // A single u32 in this buffer is a word of size 32 bits
-        let mut chunk = [0; BUFFER_SIZE_U32];
+        let mut chunk = [0; BUFFER_SIZE_U64];
 
         // println!( "INFO: created a temporary buffer of len: {}", temp_block_buf.len());
         // read message data into temporary buffer.
@@ -230,7 +253,7 @@ impl AhsahHasher for Sha256 {
 
         for i in (0..self.data.len()).step_by(BUFFER_SIZE_U8) {
             // copy into active block buffer
-            Self::copy_buf_u8_to_u32(&mut self.data, &mut chunk, i);
+            Self::copy_buf_u8_to_u64(&mut self.data, &mut chunk, i);
 
             // initialize registers
             // message schedule array
@@ -263,7 +286,7 @@ impl AhsahHasher for Sha256 {
         }
 
         format!(
-            "{:08x}{:08x}{:08x}{:08x}{:08x}{:08x}{:08x}{:08x}",
+            "{:16x}{:16x}{:16x}{:16x}{:16x}{:16x}{:16x}{:16x}",
             hash_value[0],
             hash_value[1],
             hash_value[2],
