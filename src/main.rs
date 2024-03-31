9bb8c8edc9678493896e810e972eb471b791a5c0
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-03-31 17:17:38 +0530

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use ahsah::{hashes::AhsahHasher, sha256::Sha256};
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Read},
+    env::args,
 };
 
 fn main() {
     let mut hasher = Sha256::new();
-    let file_path = "res/poem.txt";
-    let file = File::open(file_path).expect("Unable to open file");
+    let file_path = match args().skip(1).next() {
+        Some(val) => val,
+        None => String::from("res/poem.txt"),
+    };
+    let file = File::open(&file_path).expect("Unable to open file");
     let buf_reader = BufReader::new(file);
     for line in buf_reader.lines() {
         let line = line.expect("Unable to read line");
