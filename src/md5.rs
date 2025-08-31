f6dc257477712583a535b5ebfdbe8aa4853d88aa
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2025-09-01 02:33:03 +0530

    Update to use state

--- src/md5.rs
+++ src/md5.rs
@@ -50,6 +50,26 @@ const H: [u32; 4] = [
 //     0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
 //     0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
 // ];
+//
+
+#[derive(Copy, Clone)]
+struct State {
+    a: u32,
+    b: u32,
+    c: u32,
+    d: u32,
+}
+
+impl From<[u32; HASH_SIZE_U32]> for State {
+    fn from(value: [u32; HASH_SIZE_U32]) -> Self {
+        Self {
+            a: value[0],
+            b: value[1],
+            c: value[2],
+            d: value[3],
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct MD5 {
@@ -138,84 +158,83 @@ impl MD5 {
         temp_block_buf.extend_from_slice(&len.to_le_bytes());
     }
 
-    fn compression(
-        w: &[u32; MESSAGE_SCHEDULE_SIZE],
-        a: &mut u32,
-        b: &mut u32,
-        c: &mut u32,
-        d: &mut u32,
-    ) {
+    fn compress_scaler_rounds(w: &[u32; MESSAGE_SCHEDULE_SIZE], st: &mut State) {
         /* Round 1 */
-        Self::ff(a, b, c, d, w[0], S[0], 0xd76aa478); /* 1 */
-        Self::ff(d, a, b, c, w[1], S[1], 0xe8c7b756); /* 2 */
-        Self::ff(c, d, a, b, w[2], S[2], 0x242070db); /* 3 */
-        Self::ff(b, c, d, a, w[3], S[3], 0xc1bdceee); /* 4 */
-        Self::ff(a, b, c, d, w[4], S[4], 0xf57c0faf); /* 5 */
-        Self::ff(d, a, b, c, w[5], S[5], 0x4787c62a); /* 6 */
-        Self::ff(c, d, a, b, w[6], S[6], 0xa8304613); /* 7 */
-        Self::ff(b, c, d, a, w[7], S[7], 0xfd469501); /* 8 */
-        Self::ff(a, b, c, d, w[8], S[8], 0x698098d8); /* 9 */
-        Self::ff(d, a, b, c, w[9], S[9], 0x8b44f7af); /* 10 */
-        Self::ff(c, d, a, b, w[10], S[10], 0xffff5bb1); /* 11 */
-        Self::ff(b, c, d, a, w[11], S[11], 0x895cd7be); /* 12 */
-        Self::ff(a, b, c, d, w[12], S[12], 0x6b901122); /* 13 */
-        Self::ff(d, a, b, c, w[13], S[13], 0xfd987193); /* 14 */
-        Self::ff(c, d, a, b, w[14], S[14], 0xa679438e); /* 15 */
-        Self::ff(b, c, d, a, w[15], S[15], 0x49b40821); /* 16 */
+        let (mut a, mut b, mut c, mut d) = (st.a, st.b, st.c, st.d);
+        Self::ff(&mut a, &b, &c, &d, w[0], S[0], 0xd76aa478); /* 1 */
+        Self::ff(&mut d, &a, &b, &c, w[1], S[1], 0xe8c7b756); /* 2 */
+        Self::ff(&mut c, &d, &a, &b, w[2], S[2], 0x242070db); /* 3 */
+        Self::ff(&mut b, &c, &d, &a, w[3], S[3], 0xc1bdceee); /* 4 */
+        Self::ff(&mut a, &b, &c, &d, w[4], S[4], 0xf57c0faf); /* 5 */
+        Self::ff(&mut d, &a, &b, &c, w[5], S[5], 0x4787c62a); /* 6 */
+        Self::ff(&mut c, &d, &a, &b, w[6], S[6], 0xa8304613); /* 7 */
+        Self::ff(&mut b, &c, &d, &a, w[7], S[7], 0xfd469501); /* 8 */
+        Self::ff(&mut a, &b, &c, &d, w[8], S[8], 0x698098d8); /* 9 */
+        Self::ff(&mut d, &a, &b, &c, w[9], S[9], 0x8b44f7af); /* 10 */
+        Self::ff(&mut c, &d, &a, &b, w[10], S[10], 0xffff5bb1); /* 11 */
+        Self::ff(&mut b, &c, &d, &a, w[11], S[11], 0x895cd7be); /* 12 */
+        Self::ff(&mut a, &b, &c, &d, w[12], S[12], 0x6b901122); /* 13 */
+        Self::ff(&mut d, &a, &b, &c, w[13], S[13], 0xfd987193); /* 14 */
+        Self::ff(&mut c, &d, &a, &b, w[14], S[14], 0xa679438e); /* 15 */
+        Self::ff(&mut b, &c, &d, &a, w[15], S[15], 0x49b40821); /* 16 */
 
         /* Round 2 */
-        Self::gg(a, b, c, d, w[1], S[16], 0xf61e2562); /* 17 */
-        Self::gg(d, a, b, c, w[6], S[17], 0xc040b340); /* 18 */
-        Self::gg(c, d, a, b, w[11], S[18], 0x265e5a51); /* 19 */
-        Self::gg(b, c, d, a, w[0], S[19], 0xe9b6c7aa); /* 20 */
-        Self::gg(a, b, c, d, w[5], S[20], 0xd62f105d); /* 21 */
-        Self::gg(d, a, b, c, w[10], S[21], 0x2441453); /* 22 */
-        Self::gg(c, d, a, b, w[15], S[22], 0xd8a1e681); /* 23 */
-        Self::gg(b, c, d, a, w[4], S[23], 0xe7d3fbc8); /* 24 */
-        Self::gg(a, b, c, d, w[9], S[24], 0x21e1cde6); /* 25 */
-        Self::gg(d, a, b, c, w[14], S[25], 0xc33707d6); /* 26 */
-        Self::gg(c, d, a, b, w[3], S[26], 0xf4d50d87); /* 27 */
-        Self::gg(b, c, d, a, w[8], S[27], 0x455a14ed); /* 28 */
-        Self::gg(a, b, c, d, w[13], S[28], 0xa9e3e905); /* 29 */
-        Self::gg(d, a, b, c, w[2], S[29], 0xfcefa3f8); /* 30 */
-        Self::gg(c, d, a, b, w[7], S[30], 0x676f02d9); /* 31 */
-        Self::gg(b, c, d, a, w[12], S[31], 0x8d2a4c8a); /* 32 */
+        Self::gg(&mut a, &b, &c, &d, w[1], S[16], 0xf61e2562); /* 17 */
+        Self::gg(&mut d, &a, &b, &c, w[6], S[17], 0xc040b340); /* 18 */
+        Self::gg(&mut c, &d, &a, &b, w[11], S[18], 0x265e5a51); /* 19 */
+        Self::gg(&mut b, &c, &d, &a, w[0], S[19], 0xe9b6c7aa); /* 20 */
+        Self::gg(&mut a, &b, &c, &d, w[5], S[20], 0xd62f105d); /* 21 */
+        Self::gg(&mut d, &a, &b, &c, w[10], S[21], 0x2441453); /* 22 */
+        Self::gg(&mut c, &d, &a, &b, w[15], S[22], 0xd8a1e681); /* 23 */
+        Self::gg(&mut b, &c, &d, &a, w[4], S[23], 0xe7d3fbc8); /* 24 */
+        Self::gg(&mut a, &b, &c, &d, w[9], S[24], 0x21e1cde6); /* 25 */
+        Self::gg(&mut d, &a, &b, &c, w[14], S[25], 0xc33707d6); /* 26 */
+        Self::gg(&mut c, &d, &a, &b, w[3], S[26], 0xf4d50d87); /* 27 */
+        Self::gg(&mut b, &c, &d, &a, w[8], S[27], 0x455a14ed); /* 28 */
+        Self::gg(&mut a, &b, &c, &d, w[13], S[28], 0xa9e3e905); /* 29 */
+        Self::gg(&mut d, &a, &b, &c, w[2], S[29], 0xfcefa3f8); /* 30 */
+        Self::gg(&mut c, &d, &a, &b, w[7], S[30], 0x676f02d9); /* 31 */
+        Self::gg(&mut b, &c, &d, &a, w[12], S[31], 0x8d2a4c8a); /* 32 */
 
         /* Round 3 */
-        Self::hh(a, b, c, d, w[5], S[32], 0xfffa3942); /* 33 */
-        Self::hh(d, a, b, c, w[8], S[33], 0x8771f681); /* 34 */
-        Self::hh(c, d, a, b, w[11], S[34], 0x6d9d6122); /* 35 */
-        Self::hh(b, c, d, a, w[14], S[35], 0xfde5380c); /* 36 */
-        Self::hh(a, b, c, d, w[1], S[36], 0xa4beea44); /* 37 */
-        Self::hh(d, a, b, c, w[4], S[37], 0x4bdecfa9); /* 38 */
-        Self::hh(c, d, a, b, w[7], S[38], 0xf6bb4b60); /* 39 */
-        Self::hh(b, c, d, a, w[10], S[39], 0xbebfbc70); /* 40 */
-        Self::hh(a, b, c, d, w[13], S[40], 0x289b7ec6); /* 41 */
-        Self::hh(d, a, b, c, w[0], S[41], 0xeaa127fa); /* 42 */
-        Self::hh(c, d, a, b, w[3], S[42], 0xd4ef3085); /* 43 */
-        Self::hh(b, c, d, a, w[6], S[43], 0x4881d05); /* 44 */
-        Self::hh(a, b, c, d, w[9], S[44], 0xd9d4d039); /* 45 */
-        Self::hh(d, a, b, c, w[12], S[45], 0xe6db99e5); /* 46 */
-        Self::hh(c, d, a, b, w[15], S[46], 0x1fa27cf8); /* 47 */
-        Self::hh(b, c, d, a, w[2], S[47], 0xc4ac5665); /* 48 */
+        Self::hh(&mut a, &b, &c, &d, w[5], S[32], 0xfffa3942); /* 33 */
+        Self::hh(&mut d, &a, &b, &c, w[8], S[33], 0x8771f681); /* 34 */
+        Self::hh(&mut c, &d, &a, &b, w[11], S[34], 0x6d9d6122); /* 35 */
+        Self::hh(&mut b, &c, &d, &a, w[14], S[35], 0xfde5380c); /* 36 */
+        Self::hh(&mut a, &b, &c, &d, w[1], S[36], 0xa4beea44); /* 37 */
+        Self::hh(&mut d, &a, &b, &c, w[4], S[37], 0x4bdecfa9); /* 38 */
+        Self::hh(&mut c, &d, &a, &b, w[7], S[38], 0xf6bb4b60); /* 39 */
+        Self::hh(&mut b, &c, &d, &a, w[10], S[39], 0xbebfbc70); /* 40 */
+        Self::hh(&mut a, &b, &c, &d, w[13], S[40], 0x289b7ec6); /* 41 */
+        Self::hh(&mut d, &a, &b, &c, w[0], S[41], 0xeaa127fa); /* 42 */
+        Self::hh(&mut c, &d, &a, &b, w[3], S[42], 0xd4ef3085); /* 43 */
+        Self::hh(&mut b, &c, &d, &a, w[6], S[43], 0x4881d05); /* 44 */
+        Self::hh(&mut a, &b, &c, &d, w[9], S[44], 0xd9d4d039); /* 45 */
+        Self::hh(&mut d, &a, &b, &c, w[12], S[45], 0xe6db99e5); /* 46 */
+        Self::hh(&mut c, &d, &a, &b, w[15], S[46], 0x1fa27cf8); /* 47 */
+        Self::hh(&mut b, &c, &d, &a, w[2], S[47], 0xc4ac5665); /* 48 */
 
         /* Round 4 */
-        Self::ii(a, b, c, d, w[0], S[48], 0xf4292244); /* 49 */
-        Self::ii(d, a, b, c, w[7], S[49], 0x432aff97); /* 50 */
-        Self::ii(c, d, a, b, w[14], S[50], 0xab9423a7); /* 51 */
-        Self::ii(b, c, d, a, w[5], S[51], 0xfc93a039); /* 52 */
-        Self::ii(a, b, c, d, w[12], S[52], 0x655b59c3); /* 53 */
-        Self::ii(d, a, b, c, w[3], S[53], 0x8f0ccc92); /* 54 */
-        Self::ii(c, d, a, b, w[10], S[54], 0xffeff47d); /* 55 */
-        Self::ii(b, c, d, a, w[1], S[55], 0x85845dd1); /* 56 */
-        Self::ii(a, b, c, d, w[8], S[56], 0x6fa87e4f); /* 57 */
-        Self::ii(d, a, b, c, w[15], S[57], 0xfe2ce6e0); /* 58 */
-        Self::ii(c, d, a, b, w[6], S[58], 0xa3014314); /* 59 */
-        Self::ii(b, c, d, a, w[13], S[59], 0x4e0811a1); /* 60 */
-        Self::ii(a, b, c, d, w[4], S[60], 0xf7537e82); /* 61 */
-        Self::ii(d, a, b, c, w[11], S[61], 0xbd3af235); /* 62 */
-        Self::ii(c, d, a, b, w[2], S[62], 0x2ad7d2bb); /* 63 */
-        Self::ii(b, c, d, a, w[9], S[63], 0xeb86d391); /* 64 */
+        Self::ii(&mut a, &b, &c, &d, w[0], S[48], 0xf4292244); /* 49 */
+        Self::ii(&mut d, &a, &b, &c, w[7], S[49], 0x432aff97); /* 50 */
+        Self::ii(&mut c, &d, &a, &b, w[14], S[50], 0xab9423a7); /* 51 */
+        Self::ii(&mut b, &c, &d, &a, w[5], S[51], 0xfc93a039); /* 52 */
+        Self::ii(&mut a, &b, &c, &d, w[12], S[52], 0x655b59c3); /* 53 */
+        Self::ii(&mut d, &a, &b, &c, w[3], S[53], 0x8f0ccc92); /* 54 */
+        Self::ii(&mut c, &d, &a, &b, w[10], S[54], 0xffeff47d); /* 55 */
+        Self::ii(&mut b, &c, &d, &a, w[1], S[55], 0x85845dd1); /* 56 */
+        Self::ii(&mut a, &b, &c, &d, w[8], S[56], 0x6fa87e4f); /* 57 */
+        Self::ii(&mut d, &a, &b, &c, w[15], S[57], 0xfe2ce6e0); /* 58 */
+        Self::ii(&mut c, &d, &a, &b, w[6], S[58], 0xa3014314); /* 59 */
+        Self::ii(&mut b, &c, &d, &a, w[13], S[59], 0x4e0811a1); /* 60 */
+        Self::ii(&mut a, &b, &c, &d, w[4], S[60], 0xf7537e82); /* 61 */
+        Self::ii(&mut d, &a, &b, &c, w[11], S[61], 0xbd3af235); /* 62 */
+        Self::ii(&mut c, &d, &a, &b, w[2], S[62], 0x2ad7d2bb); /* 63 */
+        Self::ii(&mut b, &c, &d, &a, w[9], S[63], 0xeb86d391); /* 64 */
+        st.a = a;
+        st.b = b;
+        st.c = c;
+        st.d = d;
         //        for i in 0..64 {
         //            let mut f: u32;
         //            let g: u32;
@@ -241,12 +260,12 @@ impl MD5 {
     }
 
     fn hash_algo(&mut self) {
-        let [mut a, mut b, mut c, mut d] = &self.hashes.clone();
-        Self::compression(&self.chunk, &mut a, &mut b, &mut c, &mut d);
-        self.hashes[0] = a.wrapping_add(self.hashes[0]);
-        self.hashes[1] = b.wrapping_add(self.hashes[1]);
-        self.hashes[2] = c.wrapping_add(self.hashes[2]);
-        self.hashes[3] = d.wrapping_add(self.hashes[3]);
+        let mut state = State::from(self.hashes);
+        Self::compress_scaler_rounds(&self.chunk, &mut state);
+        self.hashes[0] = state.a.wrapping_add(self.hashes[0]);
+        self.hashes[1] = state.b.wrapping_add(self.hashes[1]);
+        self.hashes[2] = state.c.wrapping_add(self.hashes[2]);
+        self.hashes[3] = state.d.wrapping_add(self.hashes[3]);
     }
 
     fn get_hash_string(&self) -> String {
